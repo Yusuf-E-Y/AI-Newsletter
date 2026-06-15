757041b95b1b4c8baf822f0eb2e9b431c5b58aee
Yusuf <yusufefeyesil20@gmail.com>
2026-06-15 21:20:50 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,26 @@
+import React from 'react';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import HeroSection from './components/HeroSection';
+import FeaturedArticlesSection from './components/FeaturedArticlesSection';
+import TopicsBrowseSection from './components/TopicsBrowseSection';
+import NewsletterSection from './components/NewsletterSection';
+import LatestArticlesSection from './components/LatestArticlesSection';
+import { ARTICLES, TOPICS } from '@/lib/data';
 
+export default function HomePage() {
+  const featuredArticles = ARTICLES?.filter((a) => a?.featured);
+  const latestArticles = ARTICLES?.filter((a) => !a?.featured)?.slice(0, 6);
+
+  return (
+    <main className="bg-background min-h-screen">
+      <Header />
+      <HeroSection article={ARTICLES?.[0]} />
+      <FeaturedArticlesSection articles={featuredArticles} allArticles={ARTICLES?.slice(0, 4)} />
+      <TopicsBrowseSection topics={TOPICS} articles={ARTICLES} />
+      <NewsletterSection />
+      <LatestArticlesSection articles={latestArticles} />
+      <Footer />
+    </main>
+  );
+}
